2ea287f8d6493740e9c7ed751f6dfd51a2849613
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-05-18 04:05:12 +0600

    functionality implemented for adding figurama-secret-token to the local  storage in AuthProvider.jsx within the  onAuthStateChanged firebase method

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -44,6 +44,29 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             setLoading(false);
+
+            if (currentUser && currentUser.email) {
+                const loggedUserEmail = {
+                    email: currentUser.email
+                }
+
+                fetch('http://localhost:5000/jwt', {
+                    method: "POST",
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(loggedUserEmail)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('jwt response', data)
+                        // local storage is second best place to store the access token
+                        localStorage.setItem('figurama-secret-token', data.token);
+                    })
+            }
+            else {
+                localStorage.removeItem('figurama-secret-token');
+            }
         });
 
         return unsubscribe();
