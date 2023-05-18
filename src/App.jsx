717f9160f473b501f8fa8658d8d4a04df6f859a9
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-05-18 21:18:45 +0600

    react bootstrap installed for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/Shared/NavBar/NavBar';
 import Footer from './Components/Shared/Footer/Footer';
 
